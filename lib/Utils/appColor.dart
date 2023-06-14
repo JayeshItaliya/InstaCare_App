607e2fa23e3b5d0ff6379564c5e47bf8ef7199bd
JayeshItaliya <jayeshmitaliya@gmail.com>
2023-06-14 15:59:37 +0530

    init

--- lib/Utils/appColor.dart
+++ lib/Utils/appColor.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 class AppColors {
   static const Color white = Color.fromRGBO(255, 255, 255, 1);
   static const Color blue = Color.fromRGBO(22, 71, 142, 1);
@@ -8,9 +7,9 @@ class AppColors {
   static const Color backGroundColor = Color.fromRGBO(243, 250, 253, 1);
   static const Color black = Color.fromRGBO(0, 0, 0, 1);
   static const Color hintTextGrey = Color.fromRGBO(2, 5, 10, 0.5);
+
   static const Color greenDark = Color.fromRGBO(126, 209, 230, 0.2);
   static const Color deepGreen = Color.fromRGBO(126, 230, 155, 1);
   static const Color allGray = Color.fromRGBO(196, 196, 196, 1);
   static const Color redColor = Color.fromRGBO(243, 48, 71, 1);
-
 }
